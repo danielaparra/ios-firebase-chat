9c30e71d869d1d20d5c1a21727590a55fdc54d69
Daniela Parra <dyparra47@gmail.com>
2018-10-23 21:17:56 -0400

    Add firebase pod, message kit and set up storyboard.

---
+++ FirebaseChat/FirebaseChat/Model/ChatRooms.swift
@@ -0,0 +1,14 @@
+
+//
+//  ChatRooms.swift
+//  FirebaseChat
+//
+//  Created by Daniela Parra on 10/23/18.
+//  Copyright © 2018 Daniela Parra. All rights reserved.
+//
+
+import Foundation
+
+class ChatRooms {
+    
+}
---
+++ FirebaseChat/FirebaseChat/View Controllers/ChatRoomDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatRoomDetailViewController.swift
+//  FirebaseChat
+//
+//  Created by Daniela Parra on 10/23/18.
+//  Copyright © 2018 Daniela Parra. All rights reserved.
+//
+
+import UIKit
+
+class ChatRoomDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
